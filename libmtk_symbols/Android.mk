1a01c135f45f6ec67af392c92891b6a0c1f95267
rote66 <pontifex23872388@gmail.com>
2020-05-01 03:17:59 +0800

    fix symbol 1/3

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -7,6 +7,8 @@ LOCAL_SRC_FILES := \
     mtk_omx.cpp \
     icu55.c \
     icu53.c \
+    net_utils.c \
+    program_binary_builder.cpp \
     ssl.c
 
 # only for 32bit libraries
@@ -15,7 +17,11 @@ LOCAL_SRC_FILES_32 := mtk_string.cpp
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 
 LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
-                          libicuuc libicui18n libcrypto
+                          libicuuc libicui18n libcrypto libmedia libcutils libstagefright_foundation libssl libc libaudiopolicymanagerdefault libdpframework libbase
+                          
+LOCAL_STATIC_LIBRARIES := \
+    libaudiopolicycomponents
+LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
